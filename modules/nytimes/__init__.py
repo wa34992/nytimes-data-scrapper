b96fa6d9264ceede1f851a88e2f5b97ae54635ed
Waqas Ahmed <wa34992@mail.com>
2023-09-01 18:59:04 +0500

    remove logger handler.

--- modules/nytimes/__init__.py
+++ modules/nytimes/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 # Generating logs
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler('app.log', 'w', 'utf-8')
-handler.setFormatter(logging.Formatter('%(process)d-%(levelname)s-%(message)s'))
-logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
+# handler = logging.FileHandler('app.log', 'w', 'utf-8')
+# handler.setFormatter(logging.Formatter('%(process)d-%(levelname)s-%(message)s'))
+# logger.addHandler(handler)
